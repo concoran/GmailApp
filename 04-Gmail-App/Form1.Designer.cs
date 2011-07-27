bbd281330c2f4b5f2a726f402885b8ae3a4fab47
concoran <concoran@hotmail.com>
2011-07-27 14:48:28 +0530

    first commit

---
+++ 04-Gmail-App/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace _04_Gmail_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxUsr = new System.Windows.Forms.TextBox();
+            this.textBoxPwd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDispName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxBody.Location = new System.Drawing.Point(12, 137);
+            this.textBoxBody.Multiline = true;
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.Size = new System.Drawing.Size(490, 267);
+            this.textBoxBody.TabIndex = 5;
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTo.Location = new System.Drawing.Point(98, 75);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(404, 22);
+            this.textBoxTo.TabIndex = 3;
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSubject.Location = new System.Drawing.Point(98, 106);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(404, 22);
+            this.textBoxSubject.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(402, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxUsr
+            // 
+            this.textBoxUsr.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsr.Location = new System.Drawing.Point(98, 13);
+            this.textBoxUsr.Name = "textBoxUsr";
+            this.textBoxUsr.Size = new System.Drawing.Size(172, 22);
+            this.textBoxUsr.TabIndex = 0;
+            // 
+            // textBoxPwd
+            // 
+            this.textBoxPwd.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPwd.Location = new System.Drawing.Point(348, 13);
+            this.textBoxPwd.Name = "textBoxPwd";
+            this.textBoxPwd.PasswordChar = '*';
+            this.textBoxPwd.Size = new System.Drawing.Size(149, 22);
+            this.textBoxPwd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 14);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(285, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 14);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 14);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "To";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 14);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Subject";
+            // 
+            // textBoxDispName
+            // 
+            this.textBoxDispName.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDispName.Location = new System.Drawing.Point(98, 44);
+            this.textBoxDispName.Name = "textBoxDispName";
+            this.textBoxDispName.Size = new System.Drawing.Size(172, 22);
+            this.textBoxDispName.TabIndex = 2;
+            this.textBoxDispName.Text = "like John Smith";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 14);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Display Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 451);
+            this.Controls.Add(this.textBoxDispName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPwd);
+            this.Controls.Add(this.textBoxUsr);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.textBoxBody);
+            this.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gmail Send App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxUsr;
+        private System.Windows.Forms.TextBox textBoxPwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDispName;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 04-Gmail-App/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace _04_Gmail_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(textBoxUsr.Text + "@gmail.com", textBoxDispName.Text);
+            mail.To.Add(textBoxTo.Text);
+            mail.Subject = textBoxSubject.Text;
+            mail.Body = textBoxBody.Text;
+
+            SmtpClient mailclnt = new SmtpClient("smtp.gmail.com");
+            mailclnt.Port = 587;
+            mailclnt.Credentials = new System.Net.NetworkCredential(textBoxUsr.Text, textBoxPwd.Text);
+            mailclnt.EnableSsl = true;
+
+            Clipboard.SetText(textBoxBody.Text);
+
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                mailclnt.Send(mail);
+            }
+            catch (SmtpException mailE)
+            {
+                MessageBox.Show(mailE.Message);
+            }
+
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
+
